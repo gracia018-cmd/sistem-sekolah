f86dc192f4bd089228e954ec0a804fe5c1ab4ad7
gracia018-cmd <gracia.018@ski.sch.id>
2026-04-08 02:32:48 +0700

    feat: implement student page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -4,28 +4,24 @@ namespace App\Controllers;
 class StudentController
 {
 
-  public function index()
-  {
+    public function index()
+    {
+        require_once '../app/views/students/index.php';
+    }
 
-     Require_once "../app/views/students/index.php";
-  }
+    public function create()
+    {
+        require_once '../app/views/students/create.php';
+    }
 
-  public function create()
-  {
-   Require_once "../app/views/students/create.php";
+    public function show(string $id)
+    {
+        require_once '../app/views/students/show.php';
+    }
 
-  }
-
-
-
-  public function show(string $id)
-  {
-   Require_once "../app/views/students/show.php";
-
-  }
+    public function edit(string $id)
+    {
+        require_once '../app/views/students/edit.php';
+    }
 
 }
-
-
-
-?>

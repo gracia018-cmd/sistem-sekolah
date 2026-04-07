c8a22d0b2c05429aeb688ab708a4b484f4176e22
gracia018-cmd <gracia.018@ski.sch.id>
2026-04-08 02:33:16 +0700

    feat: implement student page

--- public/index.php
+++ public/index.php
@@ -6,9 +6,11 @@ use App\Core\Router;
 $router = new Router();
 
 // Register Routes
-$router->add('GET','/students','StudentController','index');
-$router->add('GET','/students/create','StudentController','create');
-$router->add('GET','/students/{id}','StudentController','show');
+$router->add('GET', '/students', 'StudentController', 'index');
+$router->add('GET', '/students/create', 'StudentController', 'create');
+$router->add('GET', '/students/{id}', 'StudentController', 'show');
+$router->add('GET', '/students/{id}/edit', 'StudentController', 'edit');
 
-$router->run(); 
+
+$router->run();
 ?>
